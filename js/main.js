631225f331e9d7506d7b6ef6979a917eb66343d1
jan-patrick <jan.schneider@hfg-gmuend.de>
2018-02-07 15:51:22 +0100

    video src with js basic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 // for resizing
 var zoom = 100;
 
@@ -7,47 +6,27 @@ var length_waehrungen = 4000;
 var length_kaffee = 11000;
 
 // paths
-var side2 = "../html/2.html";
-var side3 = "../html/3.html";
-var side4 = "../html/4.html";
-var side5 = "../html/5.html";
-var side6 = "../html/6.html";
-var side1 = "../html/1.html";
+var path1 = "../../videos/waehrungen_jan.mp4";
 
 // used length + path
 var usedLength = length_waehrungen;
-var usedPath = side1;
-
-var path = window.location.pathname;
-var page = path.split("/").pop();
-console.log( page );
+var usedPath = path1;
 
 window.onload = setVariables;
 
 function setVariables(){
-    switch(page){
-        case "1.html":
-            usedLength=length_waehrungen;
-            usedPath=side1;
-            console.log("done");
-            break;
-        case "2.html":
-            usedLength=length_waehrungen;
-            usedPath=side1;
-            console.log("done");
-            break;
-        default:
-            console.log("Error!");
-            break;
+    var video = document.getElementById('video');
+    
+    video.setAttribute("src", path1);  
+    video.load();      
     resizeScreen();        
     setTimeout();
-    }
 }
 
 setTimeout(function(){
         console.log(usedLength);
         console.log(usedPath);
-        window.open(usedPath, null , null, true);
+        //window.open(usedPath, null , null, true);
 },usedLength);
 
 function resizeScreen() {
